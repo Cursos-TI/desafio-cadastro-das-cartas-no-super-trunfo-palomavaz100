aade941231d2329913ab335266b42d67dae587cb
palomavaz100 <202501183115@alunos.estacio.br>
2025-03-19 01:53:18 +0000

    Nivel Aventureiro (Densidade + PIB per capita)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,6 +18,10 @@ int main() {
     float PIB2;
     int pontosturisticos1;
     int pontosturisticos2;
+    float densidadepopulacional1;
+    float densidadepopulacional2;
+    float PIBcapita1;
+    float PIBcapita2;
 
     //Primeiro declarei as variaveis levando em consideração duas cartas, sendo assim determinei variaveis 1 e 2 
     //seguindo para printf, mostrando a mensagem ao usuário com qual informação o mesmo deve fornecer
@@ -44,6 +48,12 @@ int main() {
     printf ("Número de Pontos Turísticos: \n");
     scanf (" %d", &pontosturisticos1);
 
+// Calculo da Densidade e PIB per capita, conhecimentos adquiridos no nivel aventureiro
+
+    densidadepopulacional1 = populacao1 / area1;
+
+    PIBcapita1 = PIB1 / populacao1;
+
 // Após determinar os valores das variaveis 1 peço ao usuario as informações da segunda carta, sendo as variaveis 2//
 
     printf ("Adicione os dados da Carta 2 \n Nome do Estado: \n");
@@ -68,6 +78,12 @@ int main() {
     printf ("Número de Pontos Turísticos: \n");
     scanf (" %d", &pontosturisticos2);
 
+    // Calculo da Densidade e PIB per capita, conhecimentos adquiridos no nivel aventureiro
+
+    densidadepopulacional2 = populacao2 / area2;
+
+    PIBcapita2 = PIB2 / populacao2;
+
     // Em seguida o sistema deve mostrar ao usuario os dados das duas cartas, assim como requerido no desafio.//
 
     printf ("Carta 1 \n");
@@ -78,7 +94,12 @@ int main() {
     printf ("Área em Km²: %f \n",area1);
     printf ("PIB: %f \n", PIB1);
     printf ("Número de Pontos Turísticos: %d \n", pontosturisticos1);
+    printf ("Densidade Populacional: %.2f \n", densidadepopulacional1);
+    printf ("PIB per capita: %.2f \n", PIBcapita1);
     
+//Foram adicionado o printf com as informações de Densidade Populacional e PIB per capita, no Nivel Aventureiro;
+
+
     printf ("Carta 2 \n");
     printf ("Estado: %c \n", estado2);
     printf ("Código da Carta: %s \n", codigo2);
@@ -87,6 +108,8 @@ int main() {
     printf ("Área em Km²: %f \n",area2);
     printf ("PIB: %f \n", PIB2);
     printf ("Número de Pontos Turísticos: %d \n", pontosturisticos2);
+    printf ("Densidade Populacional: %.2f \n", densidadepopulacional2);
+    printf ("PIB per capita: %.2f \n", PIBcapita2);
    
    //Segue os dados utilizados em cada carta: (Dados os quais o usuario deve inserir, os quais eu mesma criei para teste do programa)
 
@@ -96,7 +119,7 @@ int main() {
   //Nome da Cidade: Salvador     
   //População: 2.418.000        
   //Área em Km²: 693 km²         
-  //PIB: 62.954.487,49           
+  //PIB: 62.954.487          
   //Pontos Turísticos: 13        
     
   //Carta 2
@@ -107,5 +130,6 @@ int main() {
   //Área em Km²: 1521 Km²
   //PIB: 829.000.000.000
   //Pontos Turísticos: 36
+
   return 0;
 }
